8b0f0fa1781f02ebe37206c4f003ae08ab5249a5
rfedorov <rodion.fedorov2018@gmail.com>
2022-09-08 00:51:46 +0300

    Update models, add new factories

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -10,7 +10,7 @@
 
         <p>
 
-            <a href="/categories/{{ $post->category->slug  }}">{{ $post->category->name }}</a>
+            By <a href="#">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug  }}">{{ $post->category->name }}</a>
         </p>
 
         <div>{!! $post->body !!}</div>
