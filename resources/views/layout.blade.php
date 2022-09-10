613a8178cdde055ae6a989c75ed02f8318205bcd
rfedorov <rodion.fedorov2018@gmail.com>
2022-09-10 19:38:19 +0300

    Improve UI

--- resources/views/layout.blade.php
+++
@@ -1,9 +0,0 @@
-<html lang="en">
-<head>
-    <link rel="stylesheet" href="/app.css">
-    <title>My Blog</title>
-</head>
-<body>
-    @yield('content')
-</body>
-</html>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,19 +1,11 @@
-@extends('layout')
+<x-layout>
+    @include('_posts-header')
 
-@section('content')
-    @foreach ($posts as $post)
-        <article>
-            <h1 class="{{ $loop->even ? 'even' : '' }}">
-                <a href="/posts/{{ $post->slug }}">
-                    {!! $post->title !!}
-                </a>
-            </h1>
-
-            <p>
-                By <a href="/authors/{{$post->author->username}}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug  }}">{{ $post->category->name }}</a>
-            </p>
-
-            <div>{{ $post->excerpt }}</div>
-        </article>
-    @endforeach
-@endsection
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        @if($posts->count())
+            <x-posts-grid :posts="$posts"/>
+        @else
+            <p class="text-center">No posts yet. Please come back later</p>
+        @endif
+    </main>
+</x-layout>
