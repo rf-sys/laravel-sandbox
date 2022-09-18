361d3ac330eb7a9410e3420357934f6fd7fbe142
rfedorov <rodion.fedorov2018@gmail.com>
2022-09-18 04:33:50 +0300

    Remove model docs

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,27 +6,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
-/**
- * App\Models\Category
- *
- * @property int $id
- * @property string $name
- * @property string $slug
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Post[] $posts
- * @property-read int|null $posts_count
- * @method static \Database\Factories\CategoryFactory factory(...$parameters)
- * @method static \Illuminate\Database\Eloquent\Builder|Category newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Category newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Category query()
- * @method static \Illuminate\Database\Eloquent\Builder|Category whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Category whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Category whereName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Category whereSlug($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Category whereUpdatedAt($value)
- * @mixin \Eloquent
- */
 class Category extends Model
 {
     use HasFactory;
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,38 +7,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-/**
- * App\Models\Post
- *
- * @property int $id
- * @property int $user_id
- * @property int $category_id
- * @property string $slug
- * @property string $title
- * @property string $excerpt
- * @property string $body
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property string|null $published_at
- * @property-read \App\Models\User|null $author
- * @property-read \App\Models\Category|null $category
- * @method static \Database\Factories\PostFactory factory(...$parameters)
- * @method static Builder|Post filter(array $filters)
- * @method static Builder|Post newModelQuery()
- * @method static Builder|Post newQuery()
- * @method static Builder|Post query()
- * @method static Builder|Post whereBody($value)
- * @method static Builder|Post whereCategoryId($value)
- * @method static Builder|Post whereCreatedAt($value)
- * @method static Builder|Post whereExcerpt($value)
- * @method static Builder|Post whereId($value)
- * @method static Builder|Post wherePublishedAt($value)
- * @method static Builder|Post whereSlug($value)
- * @method static Builder|Post whereTitle($value)
- * @method static Builder|Post whereUpdatedAt($value)
- * @method static Builder|Post whereUserId($value)
- * @mixin \Eloquent
- */
 class Post extends Model
 {
     use HasFactory;
