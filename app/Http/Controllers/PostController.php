87b83fa9454de34d8967f3b2ff23b50eb9447fa8
rfedorov <rodion.fedorov2018@gmail.com>
2022-09-22 02:06:46 +0300

    Formatting

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,11 +9,13 @@ class PostController extends Controller
 {
     public function index(): View
     {
+        $posts = Post::latest()
+            ->filter(request(['search', 'category', 'author']))
+            ->paginate(6)
+            ->withQueryString();
 
         return view('posts.index', [
-            'posts' => Post::latest()->filter(
-                request(['search', 'category', 'author'])
-            )->paginate(6)->withQueryString()
+            'posts' => $posts
         ]);
     }
 
