c458b4af903bc4cd680c028d4c8072369e86ba76
rfedorov <rodion.fedorov2018@gmail.com>
2022-10-12 17:31:38 +0300

    Add Newsletter interface and binding

--- app/Http/Controllers/NewsletterController.php
+++ app/Http/Controllers/NewsletterController.php
@@ -4,16 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Services\Newsletter;
 use Exception;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Validation\ValidationException;
 
 class NewsletterController extends Controller
 {
-    public function __invoke(Newsletter $newsletter)
+    public function __invoke(Newsletter $newsletter): RedirectResponse
     {
         request()->validate(['email' => 'required|email']);
 
         try {
-            $newsletter->subscribe(request('email'));
+            $newsletter->subscribe(request()->string('email'));
         } catch (Exception) {
             throw ValidationException::withMessages([
                 'email' => 'This email could not be added to our newsletter list.'
