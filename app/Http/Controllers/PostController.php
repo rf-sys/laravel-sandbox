c2894df6983bb95e80bd42179e38bb015859265e
rfedorov <rodion.fedorov2018@gmail.com>
2022-10-13 22:03:52 +0300

    Add ability to submit custom thumbnail

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -33,10 +33,11 @@ class PostController extends Controller
         return view('posts.create');
     }
 
-    public function store(): RedirectResponse
+    public function store()
     {
         $attributes = request()->validate([
             'title' => 'required',
+            'thumbnail' => 'required|image',
             'slug' => ['required', Rule::unique('posts', 'slug')],
             'excerpt' => 'required',
             'body' => 'required',
@@ -44,6 +45,7 @@ class PostController extends Controller
         ]);
 
         $attributes['user_id'] = auth()->id();
+        $attributes['thumbnail'] = request()->file('thumbnail')->store('thumbnails');
 
         Post::create($attributes);
 
