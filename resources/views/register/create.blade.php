81aba14f2496886daf05655a190df249a4fbfd90
rfedorov <rodion.fedorov2018@gmail.com>
2022-10-14 18:27:50 +0300

    Update register view to use new UI

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -1,68 +1,18 @@
 <x-layout>
     <section class="px-6 py-8">
-        <main class="max-w-lg mx-auto mt-10 bg-gray-100 border border-gray-200 p-6 rounded-xl">
-            <h1 class="text-center font-bold text-xl">Register!</h1>
-            <form method="POST" action="/register" class="mt-10">
-                @csrf
+        <main class="max-w-lg mx-auto mt-10">
+            <x-panel>
+                <h1 class="text-center font-bold text-xl">Register!</h1>
+                <form method="POST" action="/register" class="mt-10">
+                    @csrf
 
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="name">
-                        Name
-                    </label>
-
-                    <input class="border border-gray-400 p-2 w-full" type="text" name="name" id="name"
-                           required value="{{ old('name') }}"/>
-
-                    @error('name')
-                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                    @enderror
-                </div>
-
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="username">
-                        Username
-                    </label>
-
-                    <input class="border border-gray-400 p-2 w-full" type="text" name="username" id="username"
-                           required value="{{ old('username') }}"/>
-
-                    @error('username')
-                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                    @enderror
-                </div>
-
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="email">
-                        Email
-                    </label>
-
-                    <input class="border border-gray-400 p-2 w-full" type="email" name="email" id="email"
-                           required value="{{ old('email') }}"/>
-
-                    @error('email')
-                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                    @enderror
-                </div>
-
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="password">
-                        Password
-                    </label>
-
-                    <input class="border border-gray-400 p-2 w-full" type="password" name="password" id="password"
-                           required/>
-
-                    @error('password')
-                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                    @enderror
-                </div>
-
-                <div class="mb-6">
-                    <button type="submit" class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500">
-                        Submit
-                    </button>
-                </div>
-            </form>
+                    <x-form.input name="name"/>
+                    <x-form.input name="username"/>
+                    <x-form.input name="email" type="email"/>
+                    <x-form.input name="password" type="password"/>
+                    <x-form.button>Submit</x-form.button>
+                </form>
+            </x-panel>
         </main>
     </section>
 </x-layout>
